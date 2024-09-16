8cdac6176e22816ec81a20608e7ba577de54300a
ashraf <ashrafchekintakath@gmail.com>
2024-09-16 15:59:29 +0530

    init

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from './components/Header'
+import './css/App.css'
+function App() {
+
+  return (
+    <>
+      
+
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+  return (
+  <>
+       <div>
+            <h1 className="banner">Hello welcome</h1>
+            <p className="slogan">asdffhdsfhdkjshfdfhhk</p>
+        </div> 
+        
+  </>
+  )
+}
+
+export default Header
